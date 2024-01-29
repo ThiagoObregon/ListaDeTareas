81b916e372ceed8118ef3a620174a8236106533c
ThiagoObregon <tvo@jphlions.com>
2024-01-29 15:50:55 -0300

    Inicializacion del proyecto

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// script.js
+
+function agregarTarea() {
+    // Obtener los valores de nombre y detalle de la tarea
+    var nuevaTareaNombre = document.getElementById("nuevaTareaNombre").value;
+    var nuevaTareaDetalle = document.getElementById("nuevaTareaDetalle").value;
+
+    // Validar que ambos campos no estén vacíos
+    if (nuevaTareaNombre.trim() !== "" && nuevaTareaDetalle.trim() !== "") {
+        // Crear elementos de lista con nombre y detalle de la tarea
+        var nuevaTareaNombreElemento = document.createElement("li");
+        nuevaTareaNombreElemento.textContent = "Nombre: " + nuevaTareaNombre;
+        nuevaTareaNombreElemento.classList.add("Nombre"); // Agregar clase Nombre
+
+        var nuevaTareaDetalleElemento = document.createElement("li");
+        nuevaTareaDetalleElemento.textContent = "Detalle: " + nuevaTareaDetalle;
+        nuevaTareaDetalleElemento.classList.add("Detalle"); // Agregar clase Detalle
+
+        // Crear un elemento de lista que contendrá la tarea
+        var listaTareas = document.createElement("ul");
+
+        // Agregar los elementos de la tarea a la lista
+        listaTareas.appendChild(nuevaTareaNombreElemento);
+        listaTareas.appendChild(nuevaTareaDetalleElemento);
+
+        // Agregar la lista al contenedor de tareas
+        var contenedorTarea = document.getElementById("ContenedorTarea");
+        contenedorTarea.appendChild(listaTareas);
+
+        // Limpiar los campos de texto después de agregar la tarea
+        document.getElementById("nuevaTareaNombre").value = "";
+        document.getElementById("nuevaTareaDetalle").value = "";
+    }
+}
