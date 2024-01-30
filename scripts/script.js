ac0bcbdd9bc971e0e43a00a667fd3600539460c4
ThiagoObregon <tvo@jphlions.com>
2024-01-30 13:37:01 -0300

    Para Luchaso

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-// script.js
-
 function agregarTarea() {
     // Obtener los valores de nombre y detalle de la tarea
     var nuevaTareaNombre = document.getElementById("nuevaTareaNombre").value;
@@ -10,18 +8,28 @@ function agregarTarea() {
         // Crear elementos de lista con nombre y detalle de la tarea
         var nuevaTareaNombreElemento = document.createElement("li");
         nuevaTareaNombreElemento.textContent = "Nombre: " + nuevaTareaNombre;
-        nuevaTareaNombreElemento.classList.add("Nombre_Tarea"); // Agregar clase Nombre
-
+        nuevaTareaNombreElemento.classList.add("Nombre_Tarea", "ListaDesordenada"); // Agregar clase Nombre
 
         var nuevaTareaDetalleElemento = document.createElement("li");
         nuevaTareaDetalleElemento.textContent = "Detalle: " + nuevaTareaDetalle;
-        nuevaTareaDetalleElemento.classList.add("Detalle_Tarea"); // Agregar clase Detalle
+        nuevaTareaDetalleElemento.classList.add("Detalle_Tarea", "ListaDesordenada"); // Agregar clase Detalle
 
         // Crear un elemento de lista que contendrá la tarea
         var listaTareas = document.createElement("ul");
-        listaTareas.classList.add("Contenedor_Tarea_Inferior")
+        listaTareas.classList.add("Contenedor_Tarea_Inferior");
 
-        // Agregar los elementos de la tarea a la lista
+        // Crear un elemento de lista que contendrá el checkbox
+        var checkboxElemento = document.createElement("li");
+
+        // Crear el elemento de checkbox
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        // Agregar el checkbox al elemento de lista
+        checkboxElemento.appendChild(checkbox);
+
+        // Agregar los elementos de la tarea y el checkbox a la lista
+        listaTareas.appendChild(checkboxElemento);
         listaTareas.appendChild(nuevaTareaNombreElemento);
         listaTareas.appendChild(nuevaTareaDetalleElemento);
 
